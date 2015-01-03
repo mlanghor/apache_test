788e238bd88060b2ee65fc5d8d9d73f0214310af
Mike Langhorst <mike.langhorst@gmail.com>
2015-01-02 17:15:08 -0800

    updating to use defined type

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,43 +10,47 @@
 #
 # Sample Usage:
 #
-class apache_test (
-  $base_dir,
-  $vhost_dir = "${base_dir}/conf/vhost.d",
-  $proxy_dir = "${base_dir}/conf/proxy.d",
-  $ssl_dir   = "${base_dir}/ssl",
-  
+define apache_test (
+  $base_dir        = undef,
+  $vhost_dir       = "${base_dir}/conf/vhost.d",
+  $proxy_dir       = "${base_dir}/conf/proxy.d",
+  $ssl_dir         = "${base_dir}/ssl",
+  $package_state   = undef,
+  $package_name    = undef,
+  $user            = 'proxy',
+  $group           = 'proxy',
+  $purge_vhost_dir = true,
+  $purge_ssl_dir   = true,
+  $log_level       = 'warn',
+  $httpd_template  = 'httpd-2.2-CT.conf.erb',
 
   ) {
-  
+
   File {
     owner => 'root',
     group => 'root',
     mode  => '0755',
   }
-    
-  file { '/app':
-    ensure => directory,
-  }
-  
-  file { '/app/apache':
-    ensure => directory,
-    require => File['/app'],
-  }
-  
+
   file { $base_dir:
     ensure => directory,
-    require => File['/app/apache'],
+    #  require => File['/app/apache'],
   }
-  
+
   file { "${base_dir}/conf":
-    ensure => directory,
+    ensure  => directory,
     require => File[$base_dir],
   }
-  
+
   file { $vhost_dir:
-    ensure => directory,
+    ensure  => directory,
     require => File["${base_dir}/conf"],
   }
-  
+
+  file { "${base_dir}/conf/httpd.conf":
+    ensure  => file,
+    require => File["${base_dir}/conf"],
+    content => template("$httpd_template"),
+    mode    => '0640',
+  }
 }
